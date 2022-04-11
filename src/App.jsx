b1c24b910fcb2ff59220bf4bb0edec1da87651f1
SuiYo <912146962@qq.com>
2022-04-11 23:33:53 +0800

    feature:项目生成

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.Modules.css'
+
+export default function App() {
+  const name = 'XiaoMing'
+  return (
+    <div className="app">
+      <h3>Hello {name}</h3>
+    </div>
+  )
+}
---
+++ src/plugins/reportWebVitals/index.js
@@ -0,0 +1,17 @@
+/**
+ * @description 第三方性能测试工具
+ * 具体用法需百度
+ */
+const reportWebVitals = (onPerfEntry) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry)
+      getFID(onPerfEntry)
+      getFCP(onPerfEntry)
+      getLCP(onPerfEntry)
+      getTTFB(onPerfEntry)
+    })
+  }
+}
+
+export default reportWebVitals
